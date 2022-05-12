75fa512064b32dae3c79731835a79743b3123aaa
stegcorp <stegcorp@gmail.com>
2022-05-12 22:10:03 +0300

    ang1v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
+import {AppComponent} from './components/app/app.component';
 import {HttpClientModule} from '@angular/common/http';
+import {UsersComponent} from './components/users/users.component';
+import {RouterModule, Routes} from '@angular/router';
+import { SingleUserComponent } from './components/single-user/single-user.component';
+
+const routes: Routes = [
+  {path: 'users', component: UsersComponent},
+  {path: 'users/:id' , component: SingleUserComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    SingleUserComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
