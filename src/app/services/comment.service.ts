8980699c577975b7f20e0c54a4f2f2e0897e4acb
stegcorp <stegcorp@gmail.com>
2022-05-12 19:39:24 +0300

    ang1

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {IComment} from '../models/IComment';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  private url = 'https://jsonplaceholder.typicode.com/comments';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getComment(): Observable<IComment[]> {
+    return this
+      .http
+      .get<any[]>(this.url);
+  }
 }
